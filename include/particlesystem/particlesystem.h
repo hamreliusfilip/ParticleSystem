5c44454559721fd35e91e4473d895dce35e08991
hamreliusfilip <filha243@student.liu.se>
2023-02-25 10:48:01 +0100

    Initial Commit
    Fil struktur + Emitters, Effects def & Sim loop

---
+++ include/particlesystem/particlesystem.h
@@ -0,0 +1,29 @@
+#ifndef __PARTICLESYSTEM_H__
+#define __PARTICLESYSTEM_H__
+
+#include <vector>
+#include <math.h>
+
+#include "emitter.hpp"
+#include "effect.hpp"
+#include "particle.hpp"
+
+#include <tracy/Tracy.hpp>
+#include <rendering/window.h>
+#include "glm/vec2.hpp"
+#include "glm/vec4.hpp"
+
+using Color = glm::vec4;
+
+class ParticleSystem {
+    
+public:
+    
+    std::vector<Particle> particles;
+    
+    void createParticles(Emitter* E, float dt);
+    
+    void update(std::vector<Effect> effects, float dt);
+};
+
+#endif
